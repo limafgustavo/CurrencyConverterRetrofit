4c1f494d18bfaa11f220a2b463112fe4b10086e3
Gustavo Lima <136847064+limafgustavo@users.noreply.github.com>
2023-09-14 15:46:38 -0300

    Feat: Endpoint logic has been implemented.

--- app/src/main/java/com/example/currencyconverter/api/Endpoint.kt
+++ app/src/main/java/com/example/currencyconverter/api/Endpoint.kt
@@ -1,5 +1,16 @@
 package com.example.currencyconverter.api
 
-interface Endpoint {
 
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface Endpoint {
+    @GET("/gh/fawazahmed0/currency-api@1/latest/currencies.json")
+    fun getCurrencies(): Call<JsonObject>
+
+
+    @GET("/gh/fawazahmed0/currency-api@1/latest/currencies/{from}/{to}.json")
+    fun getCurrencyRate(@Path(value = "from", encoded = true) from:String, @Path(value = "to", encoded = true) to:String): Call<JsonObject>
 }
