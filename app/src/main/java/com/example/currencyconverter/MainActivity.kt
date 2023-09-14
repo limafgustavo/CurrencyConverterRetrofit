d8de00f67f3ecec38342ef38990bfccbb7ffcd39
Gustavo Lima <136847064+limafgustavo@users.noreply.github.com>
2023-09-14 17:01:38 -0300

    Feat: ViewBinding has been implemented.

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.currencyconverter.api.Endpoint
+import com.example.currencyconverter.databinding.ActivityMainBinding
+import com.example.currencyconverter.util.NetworkUtils
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        getCurrencies()
+    }
+
+    fun getCurrencies() {
+        val retrofitClient = NetworkUtils.getRetrofitInstance("https://cdn.jsdelivr.net/")
+        val endpoint = retrofitClient.create(Endpoint::class.java)
+
+
+        endpoint.getCurrencies().enqueue(object :
+            retrofit2.Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                var data = mutableListOf<String>()
+
+                response.body()?.keySet()?.iterator()?.forEach {
+                    data.add(it)
+                }
+                println(data.count())
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                println("Error")
+            }
+
+        })
     }
 }
